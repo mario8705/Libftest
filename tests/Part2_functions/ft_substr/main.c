ffe9446682a2099472415fcfd5e5f6d2573aa60e
Alexis Lavaud <alavaud@student.le-101.fr>
2019-10-11 17:19:03 +0200

    patched ft_substr and removed ft_memdel

--- tests/Part2_functions/ft_substr/main.c
+++ tests/Part2_functions/ft_substr/main.c
@@ -1,15 +1,15 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jtoty <jtoty@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/28 12:35:18 by jtoty             #+#    #+#             */
-/*   Updated: 2017/03/09 15:54:34 by jtoty            ###   ########.fr       */
-/*                                                                            */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: alavaud <alavaud@student.le-101.fr>        +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2017/02/28 12:35:18 by jtoty        #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/11 17:16:03 by alavaud     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
 /* ************************************************************************** */
-
 #include <stdlib.h>
 #include <unistd.h>
 #include "../../../libft.h"
@@ -35,7 +35,7 @@ int		main(int argc, const char *argv[])
 		return (0);
 	else if ((arg = atoi(argv[1])) == 1)
 	{
-		if (!(strsub = ft_strsub(str, 0, 10)))
+		if (!(strsub = ft_substr(str, 0, 10)))
 			ft_print_result("NULL");
 		else
 			ft_print_result(strsub);
@@ -44,7 +44,7 @@ int		main(int argc, const char *argv[])
 	}
 	else if (arg == 2)
 	{
-		if (!(strsub = ft_strsub(str, 7, 10)))
+		if (!(strsub = ft_substr(str, 7, 10)))
 			ft_print_result("NULL");
 		else
 			ft_print_result(strsub);
@@ -53,7 +53,7 @@ int		main(int argc, const char *argv[])
 	}
 	else if (arg == 3)
 	{
-		if (!(strsub = ft_strsub(str, 7, 0)))
+		if (!(strsub = ft_substr(str, 7, 0)))
 			ft_print_result("NULL");
 		else
 			ft_print_result(strsub);
@@ -62,7 +62,7 @@ int		main(int argc, const char *argv[])
 	}
 	else if (arg == 4)
 	{
-		if (!(strsub = ft_strsub(str, 0, 0)))
+		if (!(strsub = ft_substr(str, 0, 0)))
 			ft_print_result("NULL");
 		else
 			ft_print_result(strsub);
